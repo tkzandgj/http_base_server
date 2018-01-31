f224d993adb1c4e9344e69e81390ce03aa23cc78
tukangzheng <tukangzheng@163.com>
2018-02-01 00:06:27 +0800

    '添加注释'

--- src/main/java/com/cncnc/netty/serverbase/http/NotFoundResponse.java
+++ src/main/java/com/cncnc/netty/serverbase/http/NotFoundResponse.java
@@ -1,4 +1,29 @@
 package com.cncnc.netty.serverbase.http;
 
-public class NotFoundResponse {
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Administrator
+ * 异常类
+ */
+public class NotFoundResponse extends DefaultFullHttpResponse {
+
+    private static Logger logger = LoggerFactory.getLogger(NotFoundResponse.class);
+
+    public NotFoundResponse(String msg)  {
+        super(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND,
+                Unpooled.buffer().writeBytes(msg.getBytes()));
+        this.headers().set(HttpHeaderNames.CONTENT_LENGTH, msg.getBytes().length);
+    }
+
+    public NotFoundResponse()  {
+        super(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND);
+        this.headers().set(HttpHeaderNames.CONTENT_LENGTH, 0);
+    }
 }
