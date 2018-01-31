bfb73801b1562c79016edcb30a1c81993869f98c
tukangzheng <tukangzheng@163.com>
2018-02-01 00:06:57 +0800

    '添加新方法'

--- src/main/java/com/cncnc/netty/serverbase/initializer/ServerInit.java
+++ src/main/java/com/cncnc/netty/serverbase/initializer/ServerInit.java
@@ -1,4 +1,87 @@
 package com.cncnc.netty.serverbase.initializer;
 
-public class ServerInit {
+
+import com.cncnc.netty.serverbase.NettyServerContext;
+import com.cncnc.netty.serverbase.config.ServerConfig;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.net.ssl.SSLException;
+import java.security.cert.CertificateException;
+
+/**
+ * @author Administrator
+ * 服务器初始化
+ * 需要继承该类，并提供自定义的ChannelInitializer
+ */
+public abstract class ServerInit {
+
+    @Autowired
+    private ServerConfig serverConfig;
+
+    /**
+     * 初始化服务器
+     */
+    public void init(String... args) throws CertificateException, SSLException, InterruptedException{
+        final SslContext sslContext;
+
+        if (serverConfig.getHttpConfig().isSsl()){
+            SelfSignedCertificate selfSignedCertificate = new SelfSignedCertificate();
+            sslContext = SslContextBuilder.forServer(
+                    selfSignedCertificate.certificate(), selfSignedCertificate.privateKey()).build();
+        } else {
+            sslContext = null;
+        }
+
+        preInit(serverConfig);
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerChannelInitializer channelInitializer = getChannelInitializer(serverConfig);
+            channelInitializer.setSslContext(sslContext);
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.option(ChannelOption.SO_BACKLOG, 1024)
+                    .group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(channelInitializer);
+
+            ChannelFuture future = bootstrap.bind(NettyServerContext.getPort()).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    /**
+     * 子类可以通过继承该方法做一些自己的初始化(比如建立redis连接）
+     * @param serverConfig
+     */
+    protected void preInit(ServerConfig serverConfig){
+
+    }
+
+    /**
+     * 子类可以通过继承该方法，在连接关闭后做一些处理(比如释放redis连接）
+     * @param serverConfig
+     */
+    protected void afterClose(ServerConfig serverConfig){
+
+    }
+
+    protected abstract ServerChannelInitializer getChannelInitializer(ServerConfig serverConfig);
 }
