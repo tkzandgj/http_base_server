14670452dfb622ddf7439dbae374c3e184a594de
tukangzheng <tukangzheng@163.com>
2018-02-01 23:27:18 +0800

    '修改ServerBase代码'

--- src/main/java/com/cncnc/netty/serverbase/ServerBase.java
+++ src/main/java/com/cncnc/netty/serverbase/ServerBase.java
@@ -1,4 +1,57 @@
 package com.cncnc.netty.serverbase;
 
-public abstract class ServerBase {
+
+import com.cncnc.netty.serverbase.config.ServerConfig;
+import com.cncnc.netty.serverbase.initializer.ServerInit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * HttpServer启动类的基类
+ *
+ */
+@SpringBootApplication
+public abstract class ServerBase implements CommandLineRunner{
+
+    @Autowired
+    private ServerConfig serverConfig;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        NettyServerContext.init(serverConfig);
+
+        preInit(serverConfig);
+
+        ServerInit serverInit = getServerInitializer(serverConfig);
+        serverInit.init(strings);
+
+        beforeClose(serverConfig);
+    }
+
+
+    /**
+     * 子类实现该方法提供自定义的ServerInit实例
+     * @param serverConfig
+     * @return
+     */
+    protected abstract ServerInit getServerInitializer(ServerConfig serverConfig);
+
+
+    /**
+     * 子类实现该方法做自己的一些初始化工作
+     * @param serverConfig
+     */
+    protected void preInit(ServerConfig serverConfig){
+
+    }
+
+    /**
+     * 子类实现该方法做自己的一些资源释放工作
+     * @param serverConfig
+     */
+    protected void beforeClose(ServerConfig serverConfig){
+
+    }
 }
